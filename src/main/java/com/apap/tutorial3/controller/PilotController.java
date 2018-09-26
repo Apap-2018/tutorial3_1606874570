7794f7ef0c4f61f34a7a17df98798e49ff080854
Debora Dian Pratama <deboradian09@gmail.com>
2018-09-26 22:04:46 +0700

    commit : penambahan error handling untuk latihan

--- src/main/java/com/apap/tutorial3/controller/PilotController.java
+++ src/main/java/com/apap/tutorial3/controller/PilotController.java
@@ -2,10 +2,14 @@ package com.apap.tutorial3.controller;
 
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
 import com.apap.tutorial3.model.PilotModel;
 import com.apap.tutorial3.service.PilotService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class PilotController {
+public class PilotController implements ErrorController {
 	@Autowired
 	private PilotService pilotService;
 	
@@ -48,9 +52,13 @@ public class PilotController {
 	public String viewByLicenseNumber(@PathVariable String licenseNumber, Model model) {
 		PilotModel archive = pilotService.getPilotDetailByLicenseNumber(licenseNumber);
 		
-		if(!archive.equals(null)) 
+		if(!archive.equals(null)) {
 			model.addAttribute("pilot", archive);
 			return "view-license";
+		}
+		else {
+			return "error-500";
+		}
 		
 	}
 	
@@ -80,6 +88,28 @@ public class PilotController {
 		
 		model.addAttribute("listPilot", archive);
 		return "remove-pilot";
+	}
+
+	@RequestMapping("/error")
+	public String handleError(HttpServletRequest request) {
+	    Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+	     
+	    if (status != null) {
+	        Integer statusCode = Integer.valueOf(status.toString());
+	     
+	        if(statusCode == HttpStatus.NOT_FOUND.value()) {
+	            return "error-404";
+	        }
+	        else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+	            return "error-500";
+	        }
+	    }
+	    return "error";
+	}
+	
+	@Override
+	public String getErrorPath() {
+		return "/errror";
 	}
 	
 }
