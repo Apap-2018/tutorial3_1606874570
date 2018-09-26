db7d90da3e2ee0802932b29ccf73d15dd08e21e1
Debora Dian Pratama <deboradian09@gmail.com>
2018-09-26 21:50:11 +0700

    commit : menyelesaikan latihan

--- src/main/java/com/apap/tutorial3/controller/PilotController.java
+++ src/main/java/com/apap/tutorial3/controller/PilotController.java
@@ -6,8 +6,10 @@ import com.apap.tutorial3.model.PilotModel;
 import com.apap.tutorial3.service.PilotService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -41,4 +43,43 @@ public class PilotController {
 		model.addAttribute("listPilot", archive);
 		return "viewall-pilot";
 	}
+	
+	@RequestMapping("/pilot/view/license-number/{licenseNumber}")
+	public String viewByLicenseNumber(@PathVariable String licenseNumber, Model model) {
+		PilotModel archive = pilotService.getPilotDetailByLicenseNumber(licenseNumber);
+		
+		if(!archive.equals(null)) 
+			model.addAttribute("pilot", archive);
+			return "view-license";
+		
+	}
+	
+	@RequestMapping("/pilot/view/license-number/{licenseNumber}/fly-hour/{rev_fly_hour}")
+	public String updateFlyHour(@PathVariable String licenseNumber, @PathVariable String rev_fly_hour, Model model) {
+		PilotModel archive = pilotService.getPilotDetailByLicenseNumber(licenseNumber);
+		int rev_hour = Integer.parseInt(rev_fly_hour);
+		if(!archive.equals(null)) { 
+			archive.setFlyHour(rev_hour);
+			model.addAttribute("pilot", archive);
+			return "ubah-flyHour";
+		}
+		return "ubah-flyHour";
+	}
+	
+	@RequestMapping("/pilot/delete/id/{id_hapus}")
+	public String delete(@PathVariable String id_hapus, Model model) {
+		List<PilotModel> archive = pilotService.getPilotList();
+		PilotModel removed_pilot;
+		
+		for (PilotModel pilot : archive) {
+			if (pilot.getId().equals(id_hapus)) {
+				model.addAttribute("pilot", pilot);
+				archive.remove(pilot);
+			}
+		}
+		
+		model.addAttribute("listPilot", archive);
+		return "remove-pilot";
+	}
+	
 }
