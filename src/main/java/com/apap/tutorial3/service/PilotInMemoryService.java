c89c598cded7ef4d3969ce520e638af65c181813
Debora Dian Pratama <deboradian09@gmail.com>
2018-09-26 15:12:18 +0700

    first commit sampai method view all

---
+++ src/main/java/com/apap/tutorial3/service/PilotInMemoryService.java
@@ -0,0 +1,41 @@
+package com.apap.tutorial3.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.apap.tutorial3.model.PilotModel;
+
+import org.springframework.stereotype.Service;
+
+/*
+ * PilotInMemoryService
+ */
+@Service
+public class PilotInMemoryService implements PilotService{
+	private List<PilotModel> archivePilot;
+	
+	public PilotInMemoryService() {
+		archivePilot = new ArrayList<>();
+	}
+	
+	@Override
+	public void addPilot(PilotModel pilot) {
+		archivePilot.add(pilot);
+	}
+
+	@Override
+	public List<PilotModel> getPilotList() {
+		return archivePilot;
+	}
+
+	@Override
+	public PilotModel getPilotDetailByLicenseNumber(String licenseNumber) {
+		for(PilotModel tmp_pilot : archivePilot) {
+			if(tmp_pilot.getLicenseNumber().equals(licenseNumber)) {
+				return tmp_pilot;
+			}
+		}
+		return null;
+	}
+	
+}
